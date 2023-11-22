2e7f58f0607e40ed286779fb36f6ab62c9f81130
thuyduongtd181003@gmail.com <thuyduongtd181003@gmail.com>
2023-11-23 02:30:25 +0700

    up detected image

--- uploads_file_treamlit.py
+++ uploads_file_treamlit.py
@@ -4,16 +4,47 @@ import cv2
 import matplotlib
 matplotlib.use('Agg')
 import numpy as np
+import streamlit as st
+from PIL import Image
+import os
 
-model = YOLO("/HK1/HK1-Nam3/PYTHON/DO_AN/data/runs\detect/train5/weights/best.pt")
+def run_model(image):
+    model = YOLO("./data/runs\detect/train5/weights/best.pt")
+    im1 = Image.open(image)
+    results = model.predict(source=im1, save=True) 
+    path = results[0].save_dir
+    # save plotted images
+    return path
 
-im1 = Image.open("dataset/images/00000003.jpg")
-results = model.predict(source=im1, save=True)  # save plotted images
- 
-path_final = "/HK1/HK1-Nam3/PYTHON/DO_AN/runs/detect/predict/00000061.jpg"
-# Display the PIL image using OpenCV
-im1_cv2 = cv2.cvtColor(np.array(path_final), cv2.COLOR_RGB2BGR)
-# mỗi lần chạy là mõi lần chạy là một lần tạo predict
-cv2.imshow('test', im1_cv2)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+
+def main():
+    st.title("Face_detection_with_YOLOv8")
+
+    uploaded_file = st.file_uploader("Choose an image", type=["jpg", "jpeg", "png"])
+
+    if uploaded_file is not None:
+        # Read the image
+        save_directory = "uploaded"
+        if not os.path.exists(save_directory):
+            os.makedirs(save_directory)
+        saved_path = os.path.join(save_directory, uploaded_file.name)
+        with open(saved_path, "wb") as f:
+            f.write(uploaded_file.getvalue())
+    
+        input_image = './uploaded/' + uploaded_file.name
+
+        # Display the original image
+        # st.image(input_image, caption="Original Image", use_column_width=True)
+
+        st.text("Running YOLO model...")
+        # Run the YOLO model
+        path_image = run_model(input_image)
+        path_output_imaged = '.\\' + path_image + '\\' + uploaded_file.name
+        # path_imaged = "./runs/detect/predict12/00000004.jpg"
+
+        # Display the processed image
+        st.image(path_output_imaged, caption="Output Image", use_column_width=True)
+if __name__ == "__main__":
+    main()
+
+
